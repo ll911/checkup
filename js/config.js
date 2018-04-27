b291040926054ea9095ebce125d6233f95b683e2
Leo <leo.lou@gov.bc.ca>
2018-04-27 12:21:28 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ checkup.config = {
 	"timeframe": 1 * time.Day,
 	"refresh_interval": 60,
 	"storage": {
-		"url": "https://checkup-d.data.gov.bc.ca/"
+		"url": "https://checkup-d.data.gov.bc.ca"
 	},
 	"status_text": {
 		"healthy": "Situation Normal",
